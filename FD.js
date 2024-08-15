54cc468e6af2d225ae1658694e5aaadf58d59dcc
Prachi5791 <149710634+Prachi5791@users.noreply.github.com>
2024-08-15 22:35:54 +0530

    Add files via upload

---
+++ FD.js
@@ -0,0 +1,15 @@
+function calculateMaturityAmount(){
+
+    const principle = parseFloat(document.getElementById('principle').value);
+    const intersRate = parseFloat(document.getElementById('interestRate').value);
+    const tenure = parseFloat(document.getElementById('tenure').value);
+
+    const maturityAmount = principle + (principle *intersRate*tenure)/100;
+
+    document.getElementById('result').innerText = `Maturity Amount : ${maturityAmount.toFixed(2)}`;
+
+
+
+}
+
+document.getElementById('Calculatebtn').addEventListener('click',calculateMaturityAmount);
